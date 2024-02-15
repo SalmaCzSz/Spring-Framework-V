4bcd2b486290e0b31cc490c35a72ddc5cb942ebc
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-02-14 18:25:50 -0600

    14/02/2024 | Dependency Inyection > setter

--- dependency-inyection/src/main/java/com/devs4j/di/atributo/Coche.java
+++ dependency-inyection/src/main/java/com/devs4j/di/atributo/Coche.java
@@ -10,34 +10,24 @@ public class Coche {
 	private Integer modelo;
 	private Motor motor;
 
-	//@Autowired
-	public Coche(@Value("VW") String marca, @Value("1981") Integer modelo, Motor motor) {
-		super();
-		this.marca = marca;
-		this.modelo = modelo;
-		this.motor = motor;
-	}
-
 	public String getMarca() {
 		return marca;
 	}
-
+	@Value("VW") 
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
-
 	public Integer getModelo() {
 		return modelo;
 	}
-
+	@Value("1981") 
 	public void setModelo(Integer modelo) {
 		this.modelo = modelo;
 	}
-
 	public Motor getMotor() {
 		return motor;
 	}
-
+	@Autowired
 	public void setMotor(Motor motor) {
 		this.motor = motor;
 	}
--- dependency-inyection/src/main/java/com/devs4j/di/atributo/Motor.java
+++ dependency-inyection/src/main/java/com/devs4j/di/atributo/Motor.java
@@ -8,7 +8,10 @@ public class Motor {
 	private String marca;
 	private Integer modelo;
 	
-	public Motor(@Value("XL1") String marca, @Value("1982") Integer modelo) {
+	public Motor() {
+	}
+	
+	public Motor(String marca,  Integer modelo) {
 		this.marca = marca;
 		this.modelo = modelo;
 	}
@@ -16,12 +19,14 @@ public class Motor {
 	public String getMarca() {
 		return marca;
 	}
+	@Value("XL1") 
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
 	public Integer getModelo() {
 		return modelo;
 	}
+	@Value("1982")
 	public void setModelo(Integer modelo) {
 		this.modelo = modelo;
 	}
