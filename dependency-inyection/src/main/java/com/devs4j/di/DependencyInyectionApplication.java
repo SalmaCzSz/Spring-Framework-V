8821b37c0e89899857e7ebb94f2c8a3c2a70e885
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-02-15 10:19:58 -0600

    15/02/2024 | Qualifiers

--- dependency-inyection/src/main/java/com/devs4j/di/DependencyInyectionApplication.java
+++ dependency-inyection/src/main/java/com/devs4j/di/DependencyInyectionApplication.java
@@ -1,22 +1,22 @@
 package com.devs4j.di;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.devs4j.di.atributo.Coche;
-import com.devs4j.di.atributo.Motor;
+import com.devs4j.di.atributo.*;
+import com.devs4j.di.qualifiers.*;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
 
 	public static void main(String[] args) {
 		// crear "Coche" con Dependency Inyection
-		ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
+		/*ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
 		Coche coche = context.getBean(Coche.class);
-		System.out.println(coche);
-		
-		
+		System.out.println(coche);*/
 		
 		// crear "Coche" sin Dependency Inyection
 		// se utiliza el constructor para crear el objeto
@@ -25,5 +25,24 @@ public class DependencyInyectionApplication {
 		Coche coche = new Coche("VW", 1986, motor);
 		System.out.println(coche);
 		*/
+		
+		
+		
+		final Logger log = LoggerFactory.getLogger(DependencyInyectionApplication.class);
+		ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
+		Perro perro = context.getBean(Perro.class);
+		log.info("Objeto perro {}", perro.getNombre());	
+		
+		Pajaro pajaro = context.getBean(Pajaro.class);
+		log.info("Objeto pajaro {}", pajaro.getNombre());
+		
+		Avion avion = context.getBean(Avion.class);
+		avion.volar();
+		
+		/*Animal animal = context.getBean("pajaro", Animal.class);
+		log.info("Animal nombre = {} edad = {}", animal.getNombre(), animal.getEdad());*/
+		
+		Nido nido = context.getBean(Nido.class);
+		nido.imprimir();
 	}
 }
---
+++ dependency-inyection/src/main/java/com/devs4j/di/qualifiers/Avion.java
@@ -0,0 +1,16 @@
+package com.devs4j.di.qualifiers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Avion implements Volador {
+	private static final Logger log = LoggerFactory.getLogger(Pajaro.class);
+
+	@Override
+	public void volar() {
+		log.info("Soy un avión y estoy volando!");		
+	}
+
+}
