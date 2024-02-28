6015223ecd860de5e06cbba6a2e07d5d8bee00f6
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-02-28 10:12:47 -0600

    28/02/2024 | Spring Framework + REST > endpoint para obtener usuarios

---
+++ users-app/src/main/java/com/devs4j/users/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.devs4j.users.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.devs4j.users.models.*;
+import com.devs4j.users.services.*;
+
+@RestController
+@RequestMapping("/users") // definición del recurso
+public class UserController {
+	@Autowired
+	private UserServices userService;
+	
+	@GetMapping // handler methods, se ejecutan a traves de un método HTTP y un recurso
+	public ResponseEntity<List<User>> get(){
+		return new ResponseEntity<List<User>>(userService.getUsers(), HttpStatus.OK);
+	}
+}
--- users-app/src/main/java/com/devs4j/users/services/UserServices.java
+++ users-app/src/main/java/com/devs4j/users/services/UserServices.java
@@ -1,12 +1,31 @@
 package com.devs4j.users.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.devs4j.users.models.User;
 import com.github.javafaker.Faker;
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class UserServices {
 	@Autowired
 	private Faker faker;
+	
+	private List<User> users = new ArrayList<>();
+	
+	@PostConstruct
+	public void init() {
+		for(int i = 0; i < 100; i++) {
+			users.add(new User(faker.funnyName().name(),
+							   faker.name().username(), 
+							   faker.dragonBall().character()));
+		}
+	}
+	
+	public List<User> getUsers(){
+		return users;
+	}
 }
