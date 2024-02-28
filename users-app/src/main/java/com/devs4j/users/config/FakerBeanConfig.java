d19c5f428ec7c9169ec763ee334a85be7054d8f1
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-02-28 09:55:34 -0600

    28/02/2024 | Spring Framework + REST > configuración de Faker

---
+++ users-app/src/main/java/com/devs4j/users/config/FakerBeanConfig.java
@@ -0,0 +1,14 @@
+package com.devs4j.users.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.javafaker.Faker;
+
+@Configuration
+public class FakerBeanConfig {
+	@Bean
+	public Faker getFacker() {
+		return new Faker();
+	}
+}
---
+++ users-app/src/main/java/com/devs4j/users/services/UserServices.java
@@ -0,0 +1,12 @@
+package com.devs4j.users.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.javafaker.Faker;
+
+@Service
+public class UserServices {
+	@Autowired
+	private Faker faker;
+}
