20eef0ffd42464695843810c596c5dad7140747f
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-02-28 10:40:38 -0600

    28/02/2024 | Spring Framework + REST > endpoint para obtener usuarios por username

--- users-app/src/main/java/com/devs4j/users/services/UserServices.java
+++ users-app/src/main/java/com/devs4j/users/services/UserServices.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.devs4j.users.models.User;
 import com.github.javafaker.Faker;
 import jakarta.annotation.PostConstruct;
@@ -27,5 +30,12 @@ public class UserServices {
 	
 	public List<User> getUsers(){
 		return users;
+	}
+	
+	public User getUserByUsername(String userName) {
+		return users.stream().filter(u -> u.getUserName()
+										   .equals(userName))
+										   .findAny()
+										   .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("User %s not found", userName)));
 	}
 }
