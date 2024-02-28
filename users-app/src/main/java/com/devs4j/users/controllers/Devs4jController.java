b1266a3f93e0375e6d31751007e69fc2d404bc70
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-02-28 09:43:10 -0600

    28/02/2024 | Spring Framework + REST > definición de controllers

---
+++ users-app/src/main/java/com/devs4j/users/controllers/Devs4jController.java
@@ -0,0 +1,14 @@
+package com.devs4j.users.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Stereotype
+@RequestMapping("/hello")
+public class Devs4jController {
+	@GetMapping
+	public String helloWord() {
+		return "Hola n.n";
+	}
+}
