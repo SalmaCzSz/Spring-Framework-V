45ace7a198037d20d51e5801bc4e465234e54d1b
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-03-05 09:29:01 -0600

    05/03/2024 | JPA y H2 > Creando put y delete para la entidad roles

--- users-app/src/main/java/com/devs4j/users/controllers/RoleController.java
+++ users-app/src/main/java/com/devs4j/users/controllers/RoleController.java
@@ -4,8 +4,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,16 @@ public class RoleController {
 	@PostMapping
 	public ResponseEntity<Role> createRoles(@RequestBody Role role){
 		return new ResponseEntity<Role>(service.createRole(role), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{roleId}")
+	public ResponseEntity<Role> updateRoles(@PathVariable("roleId") Integer roleId, @RequestBody Role role){
+		return new ResponseEntity<Role>(service.updateRole(roleId, role), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{roleId}")
+	public ResponseEntity<Void> deleteRole(@PathVariable("roleId") Integer roleId){
+		service.deleteRole(roleId);
+		return new ResponseEntity<Void> (HttpStatus.NO_CONTENT);
 	}
 }
--- users-app/src/main/java/com/devs4j/users/services/RoleService.java
+++ users-app/src/main/java/com/devs4j/users/services/RoleService.java
@@ -1,9 +1,12 @@
 package com.devs4j.users.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.devs4j.users.repositories.RoleRepository;
 import com.devs4j.users.entities.Role;
@@ -19,5 +22,25 @@ public class RoleService {
 	
 	public Role createRole(Role role) {
 		return repository.save(role);
+	}
+	
+	public Role updateRole(Integer roleId, Role role) {
+		Optional<Role> result = repository.findById(roleId);
+		
+		if(result.isPresent()) {
+			return repository.save(role);
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Role id %d doesn't exists", roleId));
+		}
+	}
+	
+	public void deleteRole(Integer roleId) {
+		Optional<Role> result = repository.findById(roleId);
+		
+		if(result.isPresent()) {
+			repository.delete(result.get());
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Role id %d doesn't exists", roleId));
+		}
 	}
 }
