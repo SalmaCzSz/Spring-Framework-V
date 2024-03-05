c9dfa9a416bc7cb87a7e028193f34a99f4581cb4
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2024-03-05 10:55:49 -0600

    05/03/2024 | JPA y H2 > paginación

--- users-app/src/main/java/com/devs4j/users/controllers/UserController.java
+++ users-app/src/main/java/com/devs4j/users/controllers/UserController.java
@@ -3,11 +3,13 @@ package com.devs4j.users.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devs4j.users.entities.User;
@@ -20,8 +22,8 @@ public class UserController {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> getUsers(){
-		return new ResponseEntity<>(service.getUsers(), HttpStatus.OK);
+	public ResponseEntity<Page<User>> getUsers(@RequestParam(required = false, value = "page") int page, @RequestParam(required = false, value = "size") int size){
+		return new ResponseEntity<>(service.getUsers(page, size), HttpStatus.OK);
 	}
 	
 	@GetMapping("/{userId}")
--- users-app/src/main/java/com/devs4j/users/services/UserService.java
+++ users-app/src/main/java/com/devs4j/users/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,8 +18,8 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
-	public List<User> getUsers(){
-		return userRepository.findAll();
+	public Page<User> getUsers(int page, int size){
+		return userRepository.findAll(PageRequest.of(page, size));
 	}
 	
 	public User getUserById(Integer userId) {
